b86475099dba30aaa766f24b5c69d2e8f53f67d4
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-06-15 16:28:59 -0700

    add in jquery effect for homepage

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,19 @@
-var c=0, words=['protest smarter','be more prepared','learn your rights','support your community'];
+var words = [ ' protest smarter',  ' be more prepared',  ' learn your rights',  ' support your community'];
+var i = 0;
+var text = "This text changes";
 
-function loop(){
-  $('h2 span').delay(1000).fadeTo(300,0,function(){
-     $(this).text( words[++c%words.length] ).fadeTo(300,1,loop);
-  });
-}    
-loop();
+
+function _getChangedText() {
+  i = (i + 1) % words.length;
+  return text.replace(/This text changes/, words[i]);
+}
+function _changeText() {
+  var txt = _getChangedText();
+    var d = document.getElementById("changer")
+    d.className = "fadeOut";
+    setTimeout(function(){
+     d.className = "";
+    document.getElementById("changer").innerHTML = txt;
+}, 1000);
+}
+setInterval("_changeText()", 1800);

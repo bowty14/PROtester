332f278b5ea2fe2d0fdefb3f1bf14390f9a26971
Reid Ashwill <reidashwill@gmail.com>
2020-06-16 10:48:29 -0700

    add api calls and views to support section

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,7 +1,11 @@
 class SupportsController < ApplicationController
 
   def index 
-
+    @businesses = HTTParty.get('https://warm-harbor-14009.herokuapp.com/businesses/')
   end 
+
+  def show 
+    @business = HTTParty.get("https://warm-harbor-14009.herokuapp.com/businesses/#{params[:id]}")
+  end
 
 end

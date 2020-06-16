23db9cf55f8f204e87f168a42b5fc55f584b7a57
Reid Ashwill <reidashwill@gmail.com>
2020-06-16 16:20:58 -0700

    updating to be able to push

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,7 +1,13 @@
 class SupportsController < ApplicationController
 
   def index 
-    @businesses = HTTParty.get('https://warm-harbor-14009.herokuapp.com/businesses/')
+    @food = HTTParty.get('https://warm-harbor-14009.herokuapp.com/businesses/food')
+    @beauty = HTTParty.get('https://warm-harbor-14009.herokuapp.com/businesses/beauty')
+    @health = HTTParty.get('https://warm-harbor-14009.herokuapp.com/businesses/health')
+    @clothing = HTTParty.get('https://warm-harbor-14009.herokuapp.com/businesses/clothing')
+    @design = HTTParty.get('https://warm-harbor-14009.herokuapp.com/businesses/design')
+    @misc = HTTParty.get('https://warm-harbor-14009.herokuapp.com/businesses/misc')
+    @publishing = HTTParty.get('https://warm-harbor-14009.herokuapp.com/businesses/publishing')
   end 
 
   def show 

9eb6130150b3079b1f2ca96f66c17eb7a8ebc938
Reid Ashwill <reidashwill@gmail.com>
2020-06-16 13:54:58 -0700

    troubleshoot google maps api to show map of business locations, and style the show page, as well as style the supports#index page.

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'launchy'
   gem 'pry'
+  gem 'dotenv-rails'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :safeties
   resources :tools
   resources :donations
-  resources :supports 
+  resources :supports
 end

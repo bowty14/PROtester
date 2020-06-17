6f28165b2762c2f417704905449df998f90d65ad
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-06-17 09:05:01 -0700

    update routes and photos on tools page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
 
-  get '/pages' => 'pages#index'
-  get '/safeties' => 'safeties#index'
+  get '/' => 'pages#index'
+  get '/safety' => 'safeties#index'
   get '/tools' => 'tools#index'
-  get '/donations' => 'donations#index'
-  resources :supports 
+  get '/donate' => 'donations#index'
+  get '/support' => 'supports#index'
+  get '/support/:id' => 'supports#show'
 end
